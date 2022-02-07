1631d336eafef9bc486420b807509b590edd8b24
Abhisheq04 <abhishekst@techis.io>
2022-02-07 13:07:24 +0530

    static server add

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from re import template
 
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static files')
 STATICFILES_DIRS=[
     BASE_DIR/ 'static'
 ]
