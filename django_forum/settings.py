6cfd51629e9318115009d32de75a9772574cab44
Abhisheq04 <abhishekst@techis.io>
2022-02-08 19:00:02 +0530

    settings changed

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -84,22 +84,22 @@ WSGI_APPLICATION = 'django_forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        "ENGINE" : "django.db.backends.postgresql",
-        "NAME" : "d2oajrpukkrv1h",
-        "USER" :"ypciyxkrwwjmxv",
-        "HOST" :"ec2-54-209-221-231.compute-1.amazonaws.com",
-        "PORT" : 5432,
-        "PASSWORD" :"005cf86c6187c7b75bce8b450409f38611d57ca29cfd91f3c412af173c328450",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         "ENGINE" : "django.db.backends.postgresql",
+#         "NAME" : "d2oajrpukkrv1h",
+#         "USER" :"ypciyxkrwwjmxv",
+#         "HOST" :"ec2-54-209-221-231.compute-1.amazonaws.com",
+#         "PORT" : 5432,
+#         "PASSWORD" :"005cf86c6187c7b75bce8b450409f38611d57ca29cfd91f3c412af173c328450",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
